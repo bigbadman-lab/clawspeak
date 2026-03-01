8c6f53dfb33d1b194c0f5b1bf6f467e285ac27cc
Alex Attinger <alexattinger@Katerinas-MacBook-Pro.local>
2026-03-01 12:36:23 +0000

    feat: add launch voices pack (7 voices)

--- packages/clawspeak/src/voices/index.ts
+++ packages/clawspeak/src/voices/index.ts
@@ -3,5 +3,9 @@ import type { VoiceConfig } from '../index.js';
 import eastEnd from './builtins/east_end_londoner.json' assert { type: 'json' };
 import newYorker from './builtins/new_yorker.json' assert { type: 'json' };
 import texan from './builtins/texan.json' assert { type: 'json' };
+import scouse from './builtins/scouse.json' assert { type: 'json' };
+import glaswegian from './builtins/glaswegian.json' assert { type: 'json' };
+import geordie from './builtins/geordie.json' assert { type: 'json' };
+import dublin from './builtins/dublin.json' assert { type: 'json' };
 
-export const builtInVoices: VoiceConfig[] = [eastEnd, newYorker, texan];
+export const builtInVoices: VoiceConfig[] = [eastEnd, scouse, glaswegian, geordie, dublin, newYorker, texan];
